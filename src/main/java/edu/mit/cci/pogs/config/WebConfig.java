14c8b01ddf1dc36edfdec15445d56992975e242c
Johannes Bachhuber <jobachhu@mit.edu>
2018-03-31 02:37:25 +0200

    Configure logging

--- src/main/java/edu/mit/cci/pogs/config/WebConfig.java
+++ src/main/java/edu/mit/cci/pogs/config/WebConfig.java
@@ -6,18 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Bean
-    public FilterRegistrationBean<ResourceUrlEncodingFilter> resourceEncodingFilter() {
-        FilterRegistrationBean<ResourceUrlEncodingFilter> registrationBean =
-                new FilterRegistrationBean<>();
-        registrationBean.setFilter(new ResourceUrlEncodingFilter());
-        return registrationBean;
-    }
 
     @Bean
     public FilterRegistrationBean<ShallowEtagHeaderFilter> etagFilter() {
