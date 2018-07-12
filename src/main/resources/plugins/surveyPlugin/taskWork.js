21cd6f68efd3e50fb03f07d7c7986371d1ce4ed0
Santhu <santhameena13@gmail.com>
2018-07-11 19:25:52 -0700

    Add simple survey with text input

--- src/main/resources/plugins/surveyPlugin/taskWork.js
+++ src/main/resources/plugins/surveyPlugin/taskWork.js
@@ -2,30 +2,76 @@ class Survey {
     constructor(pogsPlugin) {
         this.pogsPlugin = pogsPlugin;
     }
-    setupHTML(surveyBluePrint){
+    setupSurvey(surveyBluePrint){
+    console.log("starting survey setup...");
+        /*
+            JSON format
+            [
+              {"question":"Survey question1?",
+                "type": "text",
+                "placeholder":"question1 placeholder",
+                "default": "whatever"
+              },
+              {"question":"Survey question2?",
+                "type": "radio/check",
+                "placeholder":"question2 placeholder"
+                "options": [...],
+                "default": "whatever"
+              },
+              {"question":"Survey question3?",
+                "type": "select",
+                "placeholder":"question3 placeholder",
+                "options": [...],
+                "default": "whatever"
+              }
+            ]
 
+        */
+        var surveyValues = $.parseJSON(surveyBluePrint);
+        var self = this;
+        $("#surveyForm").find(".form-group").each(function(index) { // grab form-group
+            var surveyValue = surveyValues[index];
+            $("#question"+index).text(surveyValue["question"]);
+            if(surveyValue["type"] == "text") {
+                var $answer = $("#answer"+index);
+                $answer.attr("placeholder", surveyValue["placeholder"]);
+                $answer.on('focusin', self.handleOnClick.bind(self));
+                $answer.on('focusout', self.handleOnBlur.bind(self));
+            }
+        });
 
     }
+
+
     broadcastReceived(message){
         var attrName = message.content.attributeName;
         if(message.sender != this.pogsPlugin.subjectId) {
             if (attrName == "focusInCell") {
-
+                   // handle on focus - add class for bg change
+            } else {
+                if (attrName.indexOf("surveyAnswer_") != -1) {
+                    var cell = attrName.replace("surveyAnswer_", "");
+                    $("#answer" + cell).val(message.content.attributeStringValue);
+                }
             }
-
         }
-        //All attributes sync(send all set attributes)
+    }
+
+    handleOnClick(event){
+        var cellIndex = parseInt($(event.target).data( "cell-reference-index"));
+        if(!isNaN(cellIndex)) {
+            this.pogsPlugin.saveCompletedTaskAttribute('focusInCell',
+                                                       "", 0.0,
+                                                       cellIndex, false);
+        }
     }
 
     handleOnBlur(event){
-
         var cellIndex = parseInt($(event.target).data( "cell-reference-index"));
         if(!isNaN(cellIndex)) {
-
-            var valueTyped = $(event.target).text().replace(/\r?\n?/g, '').trim();
-            console.log("Typed value : " + valueTyped);
-            if(valueTyped != null && ! isNaN(valueTyped)) {
-                this.pogsPlugin.saveCompletedTaskAttribute('sudokuAnswer_' + cellIndex,
+            var valueTyped = $(event.target).val().replace(/\r?\n?/g, '').trim();
+            if(valueTyped != null) {
+                this.pogsPlugin.saveCompletedTaskAttribute('surveyAnswer_' + cellIndex,
                     valueTyped, 0.0,
                     0, true);
             }
@@ -39,7 +85,7 @@ var surveyPlugin = pogs.createPlugin('surveyTaskPlugin',function(){
 
     var survey = new Survey(this);
     // get config attributes from task plugin
-    survey.setupHTML(this.getStringAttribute("surveyBluePrint"));
+    survey.setupSurvey(this.getStringAttribute("surveyBluePrint"));
     this.subscribeTaskAttributeBroadcast(survey.broadcastReceived.bind(survey))
 
 });
