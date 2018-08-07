5cfdaf99c6a4f2a61867f06a069b81b2b2eac342
Min Jian Yang <29831969+MinJianYang@users.noreply.github.com>
2018-08-06 20:15:44 -0700

    Added game grid and table layout for the Whack-a-Mole Game

--- src/main/resources/plugins/wackamolePlugin/taskWork.js
+++ src/main/resources/plugins/wackamolePlugin/taskWork.js
@@ -1,13 +1,39 @@
 class Wackamole {
     constructor(pogsPlugin) {
         this.pogsPlugin = pogsPlugin;
+        console.log(pogsPlugin);
     }
     setupGrid(title){
+        var self = this;
+
         $("#wackamoleTitle").text(title);
+
+        // Only show readyView at start
+        $("#gameColumn").children().hide();
+        $("#readyView").show();
+        $("#readyBtn").on('click',self.handleReadyOnClick.bind(self));
+
+        // Game grid appearred
+        
+
+
+
+    }
+
+    broadcastReceived(message){
+        var attrName = message.content.attributeName;
+       console.log("broadcast message: " + message);
+    }
+
+    handleReadyOnClick(event){
+        // for single player
+        console.log("clicked");
+        console.log(event);
+        $("#gameColumn").children().hide();
+        $(".whack_grid").show();
     }
 
 }
-
 
 
 
@@ -15,4 +41,6 @@ var wackamolePlugin = pogs.createPlugin('wackamoleTaskPlugin', function(){
     console.log("Wack-a-mole Plugin Loaded");
     var wackamole = new Wackamole(this);
     wackamole.setupGrid("Wack-a-mole")
+    this.subscribeTaskAttributeBroadcast(wackamole.broadcastReceived.bind(wackamole))
+
 });
