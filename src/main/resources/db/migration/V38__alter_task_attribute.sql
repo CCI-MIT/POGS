57111bcde8b910cf7e092f9ea96facc2fbc01917
Carlos Botelho <carlosbpf@gmail.com>
2018-10-25 23:19:29 -0200

    added the migration

---
+++ src/main/resources/db/migration/V38__alter_task_attribute.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `completed_task_attribute`
+ADD COLUMN `extraData` TEXT NULL AFTER `completed_task_id`;
---
+++ src/main/resources/db/migration/V39__alter_field_extra_data_name.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `completed_task_attribute`
+CHANGE COLUMN `extraData` `extra_data` TEXT NULL DEFAULT NULL ;
