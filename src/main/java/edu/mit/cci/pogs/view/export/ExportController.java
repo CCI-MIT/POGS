dad8b42359a0875fa041a3b50451cd821517d823
chandana22 <chandana22011988@gmail.com>
2018-11-12 13:06:07 -0800

    Export Data API for Session - Raw Data

--- src/main/java/edu/mit/cci/pogs/view/export/ExportController.java
+++ src/main/java/edu/mit/cci/pogs/view/export/ExportController.java
@@ -1,7 +1,10 @@
 package edu.mit.cci.pogs.view.export;
 
 import edu.mit.cci.pogs.model.dao.completedtask.CompletedTaskDao;
+import edu.mit.cci.pogs.model.dao.completedtaskscore.CompletedTaskScoreDao;
 import edu.mit.cci.pogs.model.dao.eventlog.EventLogDao;
+import edu.mit.cci.pogs.model.jooq.tables.pojos.CompletedTask;
+import edu.mit.cci.pogs.model.jooq.tables.pojos.CompletedTaskScore;
 import edu.mit.cci.pogs.model.jooq.tables.pojos.EventLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +12,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.lang.reflect.Field;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @RestController
 public class ExportController {
@@ -24,33 +30,75 @@ public class ExportController {
     @Autowired
     private CompletedTaskDao completedTaskDao;
 
-    @GetMapping("/admin/export/session/eventlog/{sessionId}")
-    public void exportSessionEventLog(HttpServletResponse response, @PathVariable("sessionId") Long sessionId)
-                            throws IOException, IllegalAccessException{
-        List<EventLog> eventLogList = eventLogDao.listLogsBySessionId(sessionId);
-        response.setContentType("text/csv");
-        response.setHeader("Content-Disposition", "attachment; filename="
-                + "SessionEventLog_" + String.valueOf(sessionId)+ "_" + LocalDateTime.now() + ".csv");
+    @Autowired
+    private CompletedTaskScoreDao completedTaskScoreDao;
 
+    @GetMapping("/admin/export/session/{sessionId}")
+    public void exportSession(HttpServletResponse response, @PathVariable("sessionId") Long sessionId){
         try {
-            Field[] attributes =  EventLog.class.getDeclaredFields();
-            writeDataToCSV(attributes,eventLogList,response);
+            response.setContentType("application/zip");
+            response.setHeader("Content-Disposition", "attachment; filename=Session Report " + new Date().toString()  + ".zip");
+
+//            Event Log Data for the Session
+            List<EventLog> eventLogList = eventLogDao.listLogsBySessionId(sessionId);
+            if (eventLogList==null)
+                throw new Exception("Event Log Data Reading Failed");
+            String eventLogFileName = "SessionEventLog_" + String.valueOf(sessionId)+ "_" + LocalDateTime.now() + ".csv";
+            eventLogFileName=eventLogFileName.replaceAll(":","_");
+            PrintWriter writer1 = new PrintWriter(new OutputStreamWriter
+                    (new FileOutputStream(eventLogFileName), "UTF-8"));
+            writer1.print(writeDataToCSV(EventLog.class.getDeclaredFields(),eventLogList));
+            writer1.close();
+
+//            Get Completed Task Ids
+            List<Long> completedTaskIds = new ArrayList<>();
+            for (EventLog eventLog:eventLogList)
+                if (!completedTaskIds.contains(eventLog.getCompletedTaskId()))
+                    completedTaskIds.add(eventLog.getCompletedTaskId());
+
+//            Completed Task Data for the Session
+            List<CompletedTask> completedTaskList = completedTaskDao.listByCompletedTaskIds(completedTaskIds);
+            if (completedTaskList==null)
+                throw new Exception("Completed Tasks Data Reading Failed");
+            String completedTaskFileName = "SessionCompletedTasks_" + String.valueOf(sessionId)+ "_" + LocalDateTime.now() + ".csv";
+            completedTaskFileName=completedTaskFileName.replaceAll(":","_");
+            PrintWriter writer2 = new PrintWriter(new OutputStreamWriter
+                    (new FileOutputStream(completedTaskFileName), "UTF-8"));
+            writer2.print(writeDataToCSV(CompletedTask.class.getDeclaredFields(),completedTaskList));
+            writer2.close();
+
+//            Get Completed Task Scores
+            List<CompletedTaskScore> completedTaskScoreList = completedTaskScoreDao.listByCompletedTasksIds(completedTaskIds);
+            String completedTaskScoreFileName = "SessionCompletedTaskScores_" + String.valueOf(sessionId)+ "_" + LocalDateTime.now() + ".csv";
+            completedTaskScoreFileName=completedTaskScoreFileName.replaceAll(":","_");
+            PrintWriter writer3 = new PrintWriter(new OutputStreamWriter
+                    (new FileOutputStream(completedTaskScoreFileName), "UTF-8"));
+            writer3.print(writeDataToCSV(CompletedTaskScore.class.getDeclaredFields(),completedTaskScoreList));
+            writer3.close();
+
+//            Compute the Zip File
+            File file1 = new File(eventLogFileName);
+            File file2 = new File(completedTaskFileName);
+            File file3 = new File(completedTaskScoreFileName);
+            filesToZip(response, file1,file2,file3);
+            file1.delete();
+            file2.delete();
+            file3.delete();
+            response.flushBuffer();
+
         } catch (Exception e) {
-            throw e;
+            e.printStackTrace();
         }
     }
 
-    private <T> void writeDataToCSV(Field[] attributes,List<T> list, HttpServletResponse response ) throws IOException,IllegalAccessException{
-        OutputStream out = response.getOutputStream();
-        // Write the header line
+    private <T> String writeDataToCSV(Field[] attributes,List<T> list) throws IllegalAccessException{
+
         StringBuilder header = new StringBuilder();
         for (Field field:attributes){
             header.append(field.getName());
             header.append(",");
         }
         header.append("\n");
-        out.write(header.toString().getBytes());
-        // Write the content
         StringBuilder line = new StringBuilder();
         for (T element : list){
             for (Field field:attributes){
@@ -59,7 +107,30 @@ public class ExportController {
             }
             line.append("\n");
         }
-        out.write(line.toString().getBytes());
-        out.flush();
+        header.append(line);
+        return String.valueOf(header);
+    }
+
+    private void filesToZip(HttpServletResponse response, File... files) throws IOException {
+        // Create a buffer for reading the files
+        byte[] buf = new byte[1024];
+        // create the ZIP file
+        ZipOutputStream out = new ZipOutputStream(response.getOutputStream());
+        // compress the files
+        for(int i=0; i<files.length; i++) {
+            FileInputStream in = new FileInputStream(files[i].getName());
+            // add ZIP entry to output stream
+            out.putNextEntry(new ZipEntry(files[i].getName()));
+            // transfer bytes from the file to the ZIP file
+            int len;
+            while((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            // complete the entry
+            out.closeEntry();
+            in.close();
+        }
+        // complete the ZIP file
+        out.close();
     }
 }
