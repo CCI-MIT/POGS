3b7ae8c3c2ae9ab9626973730b61221fb217134c
chandana22 <chandana22011988@gmail.com>
2019-01-23 20:52:07 -0800

    Jeopardy Game Plugin Folder Structure

---
+++ src/main/resources/plugins/jeopardyPlugin/taskBeforeWork.js
@@ -0,0 +1,26 @@
+var teammatez = JSON.parse(teammates);
+
+
+
+
+var subjectOrder = [];
+var alreadyIn = false;
+while(subjectOrder.length != teammatez.length ){
+
+    var x = Math.floor(
+        Math.random() * teammatez.length);
+    alreadyIn = false;
+    for(var k=0; k < subjectOrder.length; k++){
+        if(x == subjectOrder[k]){
+            alreadyIn = true;
+        }
+    }
+    if(!alreadyIn){
+        subjectOrder.push(x);
+    }
+}
+
+attributesToAddz = [{"attributeName": "gridOrder",
+    "stringValue":JSON.stringify(subjectOrder) }]
+
+attributesToAdd = JSON.stringify(attributesToAddz);
