30e5bad9dfbea84422e81008f2463d4fd699561f
chandana22 <chandana22011988@gmail.com>
2019-01-27 15:55:58 -0800

    JS Files for Jeopardy Plugin

--- src/main/resources/plugins/jeopardyPlugin/taskBeforeWork.js
+++ src/main/resources/plugins/jeopardyPlugin/taskBeforeWork.js
@@ -23,8 +23,13 @@ var usedQuestions = [];
 for (var i = 0; i<4; i++){
     questions[i] = [];
     for (var j = 0; j<20; ){
-        var q = Math.floor(Math.random() * 6) + 1;
-        if (usedQuestions.includes(q) == false){
+        var q = Math.floor(Math.random() * 300) + 1;
+        var alreadyUsed = false;
+        for (var k=0;k<usedQuestions.length;k++){
+            if (q==usedQuestions[k])
+                alreadyUsed = true;
+        }
+        if (alreadyUsed == false){
             j++;
             usedQuestions.push(q);
             questions[i].push(q);
@@ -33,6 +38,6 @@ for (var i = 0; i<4; i++){
 }
 
 attributesToAddz = [{"attributeName": "gridOrder", "stringValue":JSON.stringify(subjectOrder) },
-                   {"attributeName": "questionOrder", "stringValue":JSON.stringify(questions) }]
+                   {"attributeName": "questionBluePrint", "stringValue":JSON.stringify(questions) }]
 
 attributesToAdd = JSON.stringify(attributesToAddz);
