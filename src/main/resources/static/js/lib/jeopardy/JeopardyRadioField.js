00606b48798c8500f62bd144626fe4c27ae2910c
ap1995 <ashwinip7995@gmail.com>
2019-01-30 13:33:52 -0800

    Added machine button with correct answer with given probability

--- src/main/resources/static/js/lib/jeopardy/JeopardyRadioField.js
+++ src/main/resources/static/js/lib/jeopardy/JeopardyRadioField.js
@@ -6,38 +6,37 @@ class JeopardyRadioField extends JeopardyField {
         //open file and keep the questions in a variable
 
         this.questionSetJson = questions;
+        this.probabilities = jeopardyJson;
 
         this.localPogsPlugin = this.getPogsPlugin();
         var teammates = this.localPogsPlugin.getTeammates();
         var currentId = this.localPogsPlugin.getSubjectId();
+        console.log("Current ID "+ currentId);
         for (var i = 0; i < teammates.length; i++) {
             if (currentId == teammates[i]) {
                 if (i == 0)
-                    this.prob = jeopardyJson.prob1;
+                    this.prob = parseFloat(this.probabilities.prob1);
                 else if (i==1)
-                    this.prob = jeopardyJson.prob2;
+                    this.prob = parseFloat(this.probabilities.prob2);
                 else if (i==2)
-                    this.prob = jeopardyJson.prob3;
+                    this.prob = parseFloat(this.probabilities.prob3);
                 else
-                    this.prob = jeopardyJson.prob4;
+                    this.prob = parseFloat(this.probabilities.prob4);
             }
         }
+        console.log("This.prob "+ parseFloat(this.probabilities.prob1));
         this.setupHTML();
         this.setupHooks();
     }
 
     setupHTML() {
         let str = "";
-        str += '<div class="form-group" id="jeopardyField_' + this.index + '" style="min-width: 300px;">'
-
-
-        for (let j = 1; j < 5; j++) {
-            let result = this.questionSetJson.filter(obj => (obj.ID === j));
-            console.log(result);
-            // let question = result.values();
-            str += '<label id="question' + this.index + '" class="text-left text-dark row">' + result[0].question + '</label>'
-            str += '<div id="answer' + this.index + '">'
-            $.each(result[0].value, function (j, choice) { // setup radio question
+        for (let j = 1; j < 3; j++) {
+            this.result = this.questionSetJson.filter(obj => (obj.ID === j));
+            // console.log(this.result);
+            str += '<div id="question-answer-machine"><label id="question' + this.index + '" class="text-left text-dark row">' + this.result[0].question + '</label>'
+            str += '<div id="answer' + this.index + '" class="col-8">'
+            $.each(this.result[0].value, function (j, choice) { // setup radio question
                 str += '<div class="form-check form-inline row">'
                 str += '  <label class="form-check-label text-left text-dark">'
                 str +=
@@ -47,25 +46,59 @@ class JeopardyRadioField extends JeopardyField {
                 str += '  </label> </div>'
 
             }.bind(this));
-            str += ' </div> ';
+            str += ' </div>';
             str += '<div class="text-center" id="submitAnswer">\n' +
-                '                    <p>Please click the button below when you are ready</p>\n' +
-                '                    <br>\n' +
-                '                        <div class="form-group form-inline form-row justify-content-center">\n' +
-                '                            <button type="button" class="btn btn-light" id="submitButton">Submit Answer</button>\n' +
-                '                        </div>\n' +
-                ' </div>'
+                '        <p>Please click the button below when you are ready</p>\n' +
+                '        <br>\n' +
+                '        <div class="form-group form-inline form-row justify-content-center">\n' +
+                '             <button type="button" class="btn btn-light" id="submitButton">Submit Answer</button>\n' +
+                '        </div>\n' +
+                ' </div>';
             $("submitAnswer").bind(this);
             str += this.getInteractionIndicatorHTML();
-            str += '</div> <br>';
-            $('#jeopardyForm').append(str);
-        }
 
+
+            str += '<div class="form-group" id="jeopardyField_' + this.index + '" style="min-width: 300px;">'
+            str+= '<div class="row"> ';
+            str += '<div class="text-center text-dark col-4" id="askMachine">\n' +
+                '      <p>Click the button below to ask the Machine. Remember that asking the machine subtracts one point from your score.</p>\n' +
+                '      <br>\n' +
+                '           <div class="form-group form-inline form-row justify-content-center">\n' +
+                '               <button type="button" class="btn btn-default" id="askMachineButton">Ask Machine</button>\n' +
+                '           </div>\n' +
+                '   </div>';
+            $("askMachine").bind(this);
+            str += '</div> <br></div>';
+        }
     }
 
     setupHooks() {
         super.setupHooks();
         $('#submitAnswer').on('click',this.handleSubmitOnClick.bind(this));
+        $('#askMachine').on('click',this.handleAskMachineOnClick.bind(this));
+    }
+
+    handleAskMachineOnClick(event){
+        let machSuggestion = "";
+        let randInt = Math.random();
+        console.log(this.result[0]);
+        if(randInt <= this.prob)
+            machSuggestion += this.result[0].Answer;
+        else
+        {
+            let nonAnswers = this.removeA(this.result[0].value, this.result[0].Answer);
+            machSuggestion += nonAnswers[Math.floor(Math.random() * 3)];
+        }
+        console.log(machSuggestion);
+        let machStr = "";
+        machStr += '<div class="text-center text-dark col-4" id="machSuggestion">\n' +
+            machSuggestion + '</div>';
+        var element = document.getElementById("machSuggestion");
+        if (element) {
+            element.innerHTML = machStr;
+        }
+        else
+            $('#jeopardyForm').append(machStr);
     }
 
     handleSubmitOnClick(event) {
@@ -98,6 +131,17 @@ class JeopardyRadioField extends JeopardyField {
             //End of round give a message
             //End of task -> Thanks
         }
+    }
+
+    removeA(arr) {
+        var what, a = arguments, L = a.length, ax;
+        while (L > 1 && arr.length) {
+            what = a[--L];
+            while ((ax= arr.indexOf(what)) !== -1) {
+                arr.splice(ax, 1);
+            }
+        }
+        return arr;
     }
 
 }
