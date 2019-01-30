386da9647fb6e7282eaa09059cace5be40a29366
chandana22 <chandana22011988@gmail.com>
2019-01-30 13:37:31 -0800

    Load questions from blueprint

--- src/main/resources/static/js/lib/jeopardy/JeopardyRadioField.js
+++ src/main/resources/static/js/lib/jeopardy/JeopardyRadioField.js
@@ -1,12 +1,16 @@
 class JeopardyRadioField extends JeopardyField {
     constructor(jeopardyReference, questionJson, jeopardyJson) {
         super(jeopardyReference, questionJson, jeopardyJson);
-        //todo
         this.index = this.registerListenerAndGetFieldId(this);
-        //open file and keep the questions in a variable
 
-        this.questionSetJson = questions;
-
+        this.result = [];
+        for (var i=0;i<questionJson.length;i++){
+            for (var j = 0; j<questionJson[i].length;j++) {
+                this.result.push(questions[questionJson[i][j]-1]);
+            }
+        }
+        console.log(this.result);
+        this.questionNumber = 0;
         this.localPogsPlugin = this.getPogsPlugin();
         var teammates = this.localPogsPlugin.getTeammates();
         var currentId = this.localPogsPlugin.getSubjectId();
@@ -14,9 +18,9 @@ class JeopardyRadioField extends JeopardyField {
             if (currentId == teammates[i]) {
                 if (i == 0)
                     this.prob = jeopardyJson.prob1;
-                else if (i==1)
+                else if (i == 1)
                     this.prob = jeopardyJson.prob2;
-                else if (i==2)
+                else if (i == 2)
                     this.prob = jeopardyJson.prob3;
                 else
                     this.prob = jeopardyJson.prob4;
@@ -29,43 +33,35 @@ class JeopardyRadioField extends JeopardyField {
     setupHTML() {
         let str = "";
         str += '<div class="form-group" id="jeopardyField_' + this.index + '" style="min-width: 300px;">'
+        // let question = result.values();
+        str += '<label id="question' + this.index + '" class="text-left text-dark row">' + this.result[this.questionNumber].question + '</label>'
+        str += '<div id="answer' + this.index + '">'
+        $.each(this.result[this.questionNumber].value, function (j, choice) { // setup radio question
+            str += '<div class="form-check form-inline row">'
+             str += '  <label class="form-check-label text-left text-dark">'
+            str +=
+                '    <input type="radio" class="form-check-input" name="answer"' + this.index
+                + '" value="' + choice + '" data-cell-reference-index="' + this.index + '">'
+                + choice
+            str += '  </label> </div>'
 
-
-        for (let j = 1; j < 5; j++) {
-            let result = this.questionSetJson.filter(obj => (obj.ID === j));
-            console.log(result);
-            // let question = result.values();
-            str += '<label id="question' + this.index + '" class="text-left text-dark row">' + result[0].question + '</label>'
-            str += '<div id="answer' + this.index + '">'
-            $.each(result[0].value, function (j, choice) { // setup radio question
-                str += '<div class="form-check form-inline row">'
-                str += '  <label class="form-check-label text-left text-dark">'
-                str +=
-                    '    <input type="radio" class="form-check-input" name="answer"' + this.index
-                    + '" value="' + choice + '" data-cell-reference-index="' + this.index + '">'
-                    + choice
-                str += '  </label> </div>'
-
-            }.bind(this));
-            str += ' </div> ';
-            str += '<div class="text-center" id="submitAnswer">\n' +
-                '                    <p>Please click the button below when you are ready</p>\n' +
-                '                    <br>\n' +
-                '                        <div class="form-group form-inline form-row justify-content-center">\n' +
-                '                            <button type="button" class="btn btn-light" id="submitButton">Submit Answer</button>\n' +
-                '                        </div>\n' +
-                ' </div>'
-            $("submitAnswer").bind(this);
-            str += this.getInteractionIndicatorHTML();
-            str += '</div> <br>';
-            $('#jeopardyForm').append(str);
-        }
-
+        }.bind(this));
+        str += ' </div> ';
+        str += '<div class="text-center" id="submitAnswer">\n' +
+            '                    <br>\n' +
+            '                        <div class="form-group form-inline form-row justify-content-center">\n' +
+            '                            <button type="button" class="btn btn-light" id="submitButton">Submit Answer</button>\n' +
+            '                        </div>\n' +
+            ' </div>'
+        $("submitAnswer").bind(this);
+        str += this.getInteractionIndicatorHTML();
+        str += '</div> <br>';
+        $('#jeopardyForm').append(str);
     }
 
     setupHooks() {
         super.setupHooks();
-        $('#submitAnswer').on('click',this.handleSubmitOnClick.bind(this));
+        $('#submitAnswer').on('click', this.handleSubmitOnClick.bind(this));
     }
 
     handleSubmitOnClick(event) {
@@ -90,10 +86,11 @@ class JeopardyRadioField extends JeopardyField {
         super.broadcastReceived(message);
         let attrName = message.content.attributeName;
 
-        if(attrName.indexOf(JEOPARDY_CONST.FIELD_NAME) != -1){ //sync radio button
+        if (attrName.indexOf(JEOPARDY_CONST.FIELD_NAME) != -1) { //sync radio button
             var question_number = attrName.replace(JEOPARDY_CONST.FIELD_NAME, "");
-            var radioButtons = $("#answer"+question_number).find("input[value='"+message.content.attributeStringValue+"']").prop("checked",true);
+            var radioButtons = $("#answer" + question_number).find("input[value='" + message.content.attributeStringValue + "']").prop("checked", true);
             this.setFinalAnswer(message.sender);
+            // this.questionNumber++;
             //Show next questions
             //End of round give a message
             //End of task -> Thanks
