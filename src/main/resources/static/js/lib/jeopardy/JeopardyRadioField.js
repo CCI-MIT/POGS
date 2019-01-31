8a328bace2fa7996f5a93f0e7b6a89b52cf8f8e9
ap1995 <ashwinip7995@gmail.com>
2019-01-30 20:39:52 -0800

    Question transition and round ending pages

--- src/main/resources/static/js/lib/jeopardy/JeopardyRadioField.js
+++ src/main/resources/static/js/lib/jeopardy/JeopardyRadioField.js
@@ -9,6 +9,7 @@ class JeopardyRadioField extends JeopardyField {
                 this.result.push(questions[questionJson[i][j] - 1]);
             }
         }
+        this.str = "";
         console.log(this.result);
         this.questionNumber = 0;
         var probabilities = jeopardyJson;
@@ -29,47 +30,47 @@ class JeopardyRadioField extends JeopardyField {
             }
         }
         this.setupHTML();
+        $('#jeopardyForm').append(this.str);
         this.setupHooks();
     }
 
     setupHTML() {
-        let str = "";
-        str += '<div class="form-group" id="jeopardyField_' + this.index + '" style="min-width: 300px;">'
+        // this.str = "";
+        this.str = '<div id = "question-answer-machine">';
+        this.str += '<div class="form-group" id="jeopardyField_' + this.index + '" style="min-width: 300px;">'
         // let question = result.values();
-        str += '<label id="question' + this.index + '" class="text-left text-dark row">' + this.result[this.questionNumber].question + '</label>'
-        str += '<div id="answer' + this.index + '">'
+        this.str += '<label id="question' + this.index + '" class="text-left text-dark row">' + this.result[this.questionNumber].question + '</label>'
+        this.str += '<div id="answer' + this.index + '">'
         $.each(this.result[this.questionNumber].value, function (j, choice) { // setup radio question
-            str += '<div class="form-check form-inline row">'
-            str += '  <label class="form-check-label text-left text-dark">'
-            str +=
+            this.str += '<div class="form-check form-inline row">'
+            this.str += '  <label class="form-check-label text-left text-dark">'
+            this.str +=
                 '    <input type="radio" class="form-check-input" name="answer"' + this.index
                 + '" value="' + choice + '" data-cell-reference-index="' + this.index + '">'
                 + choice
-            str += '  </label> </div>'
+            this.str += '  </label> </div>'
 
         }.bind(this));
-        str += ' </div> ';
-        str += '<div class="text-center" id="submitAnswer">\n' +
+        this.str += ' </div> ';
+        this.str += '<div class="text-center" id="submitAnswer">\n' +
             '                    <br>\n' +
             '                        <div class="form-group form-inline form-row justify-content-center">\n' +
             '                            <button type="button" class="btn btn-light" id="submitButton">Submit Answer</button>\n' +
             '                        </div>\n' +
             ' </div>'
         $("submitAnswer").bind(this);
-        str += this.getInteractionIndicatorHTML();
-        str += '<div class="form-group" id="jeopardyField_' + this.index + '" style="min-width: 300px;">'
-        str += '<div class="row"> ';
-        str += '<div class="text-center text-dark col-4" id="askMachine">\n' +
-            '      <p>Click the button below to ask the Machine. Remember that asking the machine subtracts one point from your score.</p>\n' +
+        this.str += this.getInteractionIndicatorHTML();
+        this.str += '<div class="form-group" id="jeopardyField_' + this.index + '" style="min-width: 300px;">'
+        this.str += '<div class="row"> ';
+        this.str += '<div class="text-center text-dark col-4" id="askMachine">\n' +
             '      <br>\n' +
             '           <div class="form-group form-inline form-row justify-content-center">\n' +
             '               <button type="button" class="btn btn-default" id="askMachineButton">Ask Machine</button>\n' +
             '           </div>\n' +
             '   </div>';
         $("askMachine").bind(this);
-        str += '</div> <br></div>';
-        // str += '</div> <br>';
-        $('#jeopardyForm').append(str);
+        this.str += '</div> <br></div>';
+        this.str += '</div> <br>';
 
     }
 
@@ -102,13 +103,9 @@ class JeopardyRadioField extends JeopardyField {
     }
 
     handleSubmitOnClick(event) {
-        // console.log("event.target " + $(event.target))
-        // let cellIndex = parseInt($(event.target).data("data-cell-reference-index"));
         let cellIndex = $('input[name="answer"]:checked').index();
         console.log("answer " + cellIndex);
         if (!isNaN(cellIndex)) {
-            // console.log($(event.target))
-            // var valueTyped = $(event.target).attr('value'); // value of radio button
             let valueTyped = $('input[name="answer"]:checked').val();
             console.log("Typed Value: " + valueTyped);
             if (valueTyped != null) {
@@ -125,10 +122,38 @@ class JeopardyRadioField extends JeopardyField {
 
         if (attrName.indexOf(JEOPARDY_CONST.FIELD_NAME) != -1) { //sync radio button
             var question_number = attrName.replace(JEOPARDY_CONST.FIELD_NAME, "");
-            var radioButtons = $("#answer" + question_number).find("input[value='" + message.content.attributeStringValue + "']").prop("checked", true);
+            // var radioButtons = $("#answer" + question_number).find("input[value='" + message.content.attributeStringValue + "']").prop("checked", true);
             this.setFinalAnswer(message.sender);
-            // this.questionNumber++;
+            this.questionNumber++;
             //Show next questions
+            console.log("Next question number " + this.questionNumber);
+
+            var questionEl = document.getElementById("question-answer-machine");
+            if (questionEl) {
+                console.log("next");
+                if (this.questionNumber === 40) {
+                    this.str = '<div id = "thankYou"> ' +
+                        '<p class = "text-dark"> End of Experiment</p>' +
+                        ' </div>';
+                    $('#jeopardyForm').append(this.str);
+                }
+                else if ((this.questionNumber + 1) % 10 === 0) {
+                    this.str = '<div id = "roundTransition"> ' +
+                        '<p class = "text-dark"> Going to the next round...</p>' +
+                        ' </div>';
+                    questionEl.innerHTML = this.str;
+                    // this.sleep(50000);
+                    // this.setupHTML();
+                    // questionEl.innerHTML = this.str;
+                    // this.setupHooks();
+                }
+                else {
+                    this.setupHTML();
+                    questionEl.innerHTML = this.str;
+                    this.setupHooks();
+                }
+
+            }
             //End of round give a message
             //End of task -> Thanks
         }
