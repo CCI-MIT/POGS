effca7b0ad5b58e553aca97f9326c188185ee5b7
chandana22 <chandana22011988@gmail.com>
2019-02-22 11:00:50 -0800

    Update to DB yaml reverting the password change

--- src/main/resources/static/js/lib/jeopardy/QuestionGenerator.js
+++ src/main/resources/static/js/lib/jeopardy/QuestionGenerator.js
@@ -3,7 +3,7 @@ var usedQuestions = [];
 
 for (var i = 0; i<4; i++){
     questions[i] = [];
-    for (var j = 0; j<20; ){
+    for (var j = 0; j<10; ){
         var q = Math.floor(Math.random() * 300) + 1;
         var alreadyUsed = false;
         for (var k=0;k<usedQuestions.length;k++){
