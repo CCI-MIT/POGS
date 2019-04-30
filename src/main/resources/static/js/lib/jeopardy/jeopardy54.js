f6c0a6e8959a031ef9a4df78740c7a376fa1a4bd
chandana22 <chandana22011988@gmail.com>
2019-04-30 13:56:17 -0700

    Jeopardy changes - Bug Fixes - Typo in questions file

--- src/main/resources/static/js/lib/jeopardy/jeopardy54.js
+++ src/main/resources/static/js/lib/jeopardy/jeopardy54.js
@@ -1,7 +1,7 @@
 this.questions = [
         {
             "ID":1
-            ,"Question":"Which kind of waves are used to make and receive cellphone calls?"
+            ,"question":"Which kind of waves are used to make and receive cellphone calls?"
             ,"Answer":"Radio Waves"
             ,"value":["Radio Waves", "Sound Waves", "Gravity Waves", "Visible Light Waves"]
             ,"Category":"Science and Technology"
@@ -9,7 +9,7 @@ this.questions = [
         },
         {
             "ID":2
-            ,"Question":"Which acid is present in lemon?"
+            ,"question":"Which acid is present in lemon?"
             ,"Answer":"citric acid"
             ,"value":["marlic acid", "lactic acid", "tartaric acid", "citric acid"]
             ,"Category":"Science and Technology"
@@ -17,7 +17,7 @@ this.questions = [
         },
         {
             "ID":3
-            ,"Question":"The intensity of Earthquakes is measured on"
+            ,"question":"The intensity of Earthquakes is measured on"
             ,"Answer":"Richter scale"
             ,"value":["Richter scale", "Mercalli scale", "Beaufort scale", "Secant scale"]
             ,"Category":"Science and Technology"
@@ -25,7 +25,7 @@ this.questions = [
         },
         {
             "ID":4
-            ,"Question":"Balloons are filled with"
+            ,"question":"Balloons are filled with"
             ,"Answer":"Helium"
             ,"value":["oxygen", "nitrogen", "argon", "Helium"]
             ,"Category":"Science and Technology"
@@ -33,7 +33,7 @@ this.questions = [
         },
         {
             "ID":5
-            ,"Question":"Along with whom did Bill Gates found Microsoft?"
+            ,"question":"Along with whom did Bill Gates found Microsoft?"
             ,"Answer":"Paul Allen"
             ,"value":["Steve Wozniack", "John Blankenbaker", "Chuck Peddle", "Paul Allen"]
             ,"Category":"Science and Technology"
@@ -41,7 +41,7 @@ this.questions = [
         },
         {
             "ID":6
-            ,"Question":"On the verge of going bust in 1997, it was saved by a $150M investment by rival Microsoft"
+            ,"question":"On the verge of going bust in 1997, it was saved by a $150M investment by rival Microsoft"
             ,"Answer":"Apple"
             ,"value":["HP", "Google", "Dell", "Apple"]
             ,"Category":"Science and Technology"
@@ -49,7 +49,7 @@ this.questions = [
         },
         {
             "ID":7
-            ,"Question":"Largest flower in the world is "
+            ,"question":"Largest flower in the world is "
             ,"Answer":"Rafflesia arnoldii"
             ,"value":["Rhododendron", "Rafflesia arnoldii", "Peniocereus", "Wolffia globosa"]
             ,"Category":"Science and Technology"
@@ -57,7 +57,7 @@ this.questions = [
         },
         {
             "ID":8
-            ,"Question":"Which country in the world has maximum number of robots working?"
+            ,"question":"Which country in the world has maximum number of robots working?"
             ,"Answer":"Japan"
             ,"value":["North Korea", "Japan", "Russia", "USA"]
             ,"Category":"Science and Technology"
@@ -65,7 +65,7 @@ this.questions = [
         },
         {
             "ID":9
-            ,"Question":"The first mechanical computer designed by Charles Babbage was called"
+            ,"question":"The first mechanical computer designed by Charles Babbage was called"
             ,"Answer":"Analytical Engine"
             ,"value":["Abacus", "Analytical Engine", "Professor", "Calculator"]
             ,"Category":"Science and Technology"
@@ -73,7 +73,7 @@ this.questions = [
         },
         {
             "ID":10
-            ,"Question":"Who was the first American to go into space in 1966?"
+            ,"question":"Who was the first American to go into space in 1966?"
             ,"Answer":"Alan Shephard"
             ,"value":["John Glenn", "Scott Carpenter", "Alan Shephard", "Neil Armstrong"]
             ,"Category":"Science and Technology"
@@ -81,7 +81,7 @@ this.questions = [
         },
         {
             "ID":11
-            ,"Question":"A baby blue whale drinks this many liters of milk per day"
+            ,"question":"A baby blue whale drinks this many liters of milk per day"
             ,"Answer":"190"
             ,"value":["190", "50", "330", "10"]
             ,"Category":"Science and Technology"
@@ -89,7 +89,7 @@ this.questions = [
         },
         {
             "ID":12
-            ,"Question":"On which planet is the tallest mountain in the Solar System?"
+            ,"question":"On which planet is the tallest mountain in the Solar System?"
             ,"Answer":"Mars"
             ,"value":["Earth", "Jupiter", "Mars", "Mercury"]
             ,"Category":"Science and Technology"
@@ -97,7 +97,7 @@ this.questions = [
         },
         {
             "ID":13
-            ,"Question":"How many men have walked on the moon?"
+            ,"question":"How many men have walked on the moon?"
             ,"Answer":"12"
             ,"value":["13", "12", "15", "14"]
             ,"Category":"Science and Technology"
@@ -105,7 +105,7 @@ this.questions = [
         },
         {
             "ID":14
-            ,"Question":"The Statue of Liberty is green because of"
+            ,"question":"The Statue of Liberty is green because of"
             ,"Answer":"oxidized copper"
             ,"value":["steel painted green", "oxidized copper", "green stone", "oxidized brass"]
             ,"Category":"Science and Technology"
@@ -113,7 +113,7 @@ this.questions = [
         },
         {
             "ID":15
-            ,"Question":"The longest bone in the human body is"
+            ,"question":"The longest bone in the human body is"
             ,"Answer":"femur"
             ,"value":["femur", "fibula", "tibia", "stapes"]
             ,"Category":"Science and Technology"
@@ -121,7 +121,7 @@ this.questions = [
         },
         {
             "ID":16
-            ,"Question":"The first conversation recounted in the Bible is in Genesis 3, between these two; it leads to trouble."
+            ,"question":"The first conversation recounted in the Bible is in Genesis 3, between these two; it leads to trouble."
             ,"Answer":"Eve and Serpent"
             ,"value":["Eve and Serpent", "Adam and Serpent", "Adam and God", "Adam and Eve"]
             ,"Category":"History and Mythology"
@@ -129,7 +129,7 @@ this.questions = [
         },
         {
             "ID":17
-            ,"Question":"He was the Confederate general in the US Civil War"
+            ,"question":"He was the Confederate general in the US Civil War"
             ,"Answer":"Robert E Lee"
             ,"value":["Robert E Lee", "Abraham Lincoln", "Jefferson Davis", "Ulysses Grant"]
             ,"Category":"History and Mythology"
@@ -137,7 +137,7 @@ this.questions = [
         },
         {
             "ID":18
-            ,"Question":"Apartheid was a system of racial separation and segregation in this African country"
+            ,"question":"Apartheid was a system of racial separation and segregation in this African country"
             ,"Answer":"South Africa"
             ,"value":["Nigeria", "Ethiopia", "South Africa", "Somalia"]
             ,"Category":"History and Mythology"
@@ -145,7 +145,7 @@ this.questions = [
         },
         {
             "ID":19
-            ,"Question":"Romulus and Remus were raised by this animal"
+            ,"question":"Romulus and Remus were raised by this animal"
             ,"Answer":"Wolf"
             ,"value":["Wolf", "Eagle", "Bull", "Snake"]
             ,"Category":"History and Mythology"
@@ -153,7 +153,7 @@ this.questions = [
         },
         {
             "ID":20
-            ,"Question":"Ferdinand Magellan, the first to sail around the world, was from this country."
+            ,"question":"Ferdinand Magellan, the first to sail around the world, was from this country."
             ,"Answer":"Portugal"
             ,"value":["Spain", "Greece", "Portugal", "France"]
             ,"Category":"History and Mythology"
@@ -161,7 +161,7 @@ this.questions = [
         },
         {
             "ID":21
-            ,"Question":"When Galveston was devastated by a hurricane in 1900, she traveled 1,500 miles to head up the relief effort."
+            ,"question":"When Galveston was devastated by a hurricane in 1900, she traveled 1,500 miles to head up the relief effort."
             ,"Answer":"Clara Barton"
             ,"value":["Florence Nightingale", "Grace Kelly", "Maya Angelou", "Clara Barton"]
             ,"Category":"History and Mythology"
@@ -169,7 +169,7 @@ this.questions = [
         },
         {
             "ID":22
-            ,"Question":"This sea route runs along the southern tip of Chile and was first navigated in 1520"
+            ,"question":"This sea route runs along the southern tip of Chile and was first navigated in 1520"
             ,"Answer":"The Strait of Magellan"
             ,"value":["Panama Canal", "English Channel", "The Straits of Gibraltar", "The Strait of Magellan"]
             ,"Category":"History and Mythology"
@@ -177,7 +177,7 @@ this.questions = [
         },
         {
             "ID":23
-            ,"Question":"In 79 A.D this volcano erupted destroying cities such as Pompeii and Herculaneum"
+            ,"question":"In 79 A.D this volcano erupted destroying cities such as Pompeii and Herculaneum"
             ,"Answer":"Mt. Vesuvius"
             ,"value":["Mt. St. Helens", "Mt. Damavand", "Koryakskaya Sopka", "Mt. Vesuvius"]
             ,"Category":"History and Mythology"
@@ -185,7 +185,7 @@ this.questions = [
         },
         {
             "ID":24
-            ,"Question":"What mythological beast has the head of a man, the body of a lion, and the tail and feet of a dragon?"
+            ,"question":"What mythological beast has the head of a man, the body of a lion, and the tail and feet of a dragon?"
             ,"Answer":"Manticore"
             ,"value":["Manticore", "Chimera", "Cyclops", "Griffin"]
             ,"Category":"History and Mythology"
@@ -193,7 +193,7 @@ this.questions = [
         },
         {
             "ID":25
-            ,"Question":"Who was the ancient Greek god of dreams?"
+            ,"question":"Who was the ancient Greek god of dreams?"
             ,"Answer":"Morpheus"
             ,"value":["Zeus", "Adonis", "Morpheus", "Artemis"]
             ,"Category":"History and Mythology"
@@ -201,7 +201,7 @@ this.questions = [
         },
         {
             "ID":26
-            ,"Question":"Before Eisenhower, he was the last president to preside over the admission of a new state."
+            ,"question":"Before Eisenhower, he was the last president to preside over the admission of a new state."
             ,"Answer":"Taft"
             ,"value":["Roosevelt", "Lincoln", "Eisenhower", "Taft"]
             ,"Category":"History and Mythology"
@@ -209,7 +209,7 @@ this.questions = [
         },
         {
             "ID":27
-            ,"Question":"This US president authorized the creation of NASA"
+            ,"question":"This US president authorized the creation of NASA"
             ,"Answer":"Dwight D Eisenhower"
             ,"value":["Richard Nixon", "Harry Truman", "John F Kennedy", "Dwight D Eisenhower"]
             ,"Category":"History and Mythology"
@@ -217,7 +217,7 @@ this.questions = [
         },
         {
             "ID":28
-            ,"Question":"This country is the world's oldest functioning democracy."
+            ,"question":"This country is the world's oldest functioning democracy."
             ,"Answer":"Iceland"
             ,"value":["Britain", "France", "Spain", "Iceland"]
             ,"Category":"History and Mythology"
@@ -225,7 +225,7 @@ this.questions = [
         },
         {
             "ID":29
-            ,"Question":"Ayutthaya is the historical former capital city of this modern-day country"
+            ,"question":"Ayutthaya is the historical former capital city of this modern-day country"
             ,"Answer":"Thailand"
             ,"value":["Malaysia", "Vietnam", "Tanzania", "Thailand"]
             ,"Category":"History and Mythology"
@@ -233,7 +233,7 @@ this.questions = [
         },
         {
             "ID":30
-            ,"Question":"In ancient Egyption mythology, who supervised the weighing of souls at judgement?"
+            ,"question":"In ancient Egyption mythology, who supervised the weighing of souls at judgement?"
             ,"Answer":"Anubis"
             ,"value":["Nephthys", "Hathor", "Horus", "Anubis"]
             ,"Category":"History and Mythology"
@@ -241,7 +241,7 @@ this.questions = [
         },
         {
             "ID":31
-            ,"Question":"He was the author of the famous storybook \"Alice in Wonderland\""
+            ,"question":"He was the author of the famous storybook \"Alice in Wonderland\""
             ,"Answer":"Lewis Carroll"
             ,"value":["Rudyard Kipling", "H G Wells", "John Keats", "Lewis Carroll"]
             ,"Category":"Literature and Media"
@@ -249,7 +249,7 @@ this.questions = [
         },
         {
             "ID":32
-            ,"Question":"A Tale of Two Cities' by Charles Dickens is based on this historical event."
+            ,"question":"A Tale of Two Cities' by Charles Dickens is based on this historical event."
             ,"Answer":"French Revolution"
             ,"value":["American Revolution", "Chinese Revolution", "Russian Revolution", "French Revolution"]
             ,"Category":"Literature and Media"
@@ -257,7 +257,7 @@ this.questions = [
         },
         {
             "ID":33
-            ,"Question":"This is Austin Powers' middle name."
+            ,"question":"This is Austin Powers' middle name."
             ,"Answer":"Danger"
             ,"value":["Ringo", "Swingin' ", "James", "Danger"]
             ,"Category":"Literature and Media"
@@ -265,7 +265,7 @@ this.questions = [
         },
         {
             "ID":34
-            ,"Question":"How many seasons of the hit US comedy ‘Friends’ were made?"
+            ,"question":"How many seasons of the hit US comedy ‘Friends’ were made?"
             ,"Answer":"10"
             ,"value":["9", "11", "12", "10"]
             ,"Category":"Literature and Media"
@@ -273,7 +273,7 @@ this.questions = [
         },
         {
             "ID":35
-            ,"Question":"Where do the Flintstones live?"
+            ,"question":"Where do the Flintstones live?"
             ,"Answer":"BedRock"
             ,"value":["Crystal Cove", "Burbank", "Kirrin", "BedRock"]
             ,"Category":"Literature and Media"
@@ -281,7 +281,7 @@ this.questions = [
         },
         {
             "ID":36
-            ,"Question":"Who is the host of \"Wheel of Fortune\"?"
+            ,"question":"Who is the host of \"Wheel of Fortune\"?"
             ,"Answer":"Pat Satjak"
             ,"value":["Alex Trebek", "Bob Barker", "Drew Carey", "Pat Satjak"]
             ,"Category":"Literature and Media"
@@ -289,7 +289,7 @@ this.questions = [
         },
         {
             "ID":37
-            ,"Question":"\"Omerta\", the title of the last book he completed before his death in 1999, is Sicilian for \"code of silence\""
+            ,"question":"\"Omerta\", the title of the last book he completed before his death in 1999, is Sicilian for \"code of silence\""
             ,"Answer":"Mario Puzo"
             ,"value":["Jeffrey Archer", "Kurt Vonnegut", "Truman Capote", "Mario Puzo"]
             ,"Category":"Literature and Media"
@@ -297,7 +297,7 @@ this.questions = [
         },
         {
             "ID":38
-            ,"Question":"One reason he is not buried in Westminster Abbey is his epitaph, which concludes, \"Curst be he that moves my bones\""
+            ,"question":"One reason he is not buried in Westminster Abbey is his epitaph, which concludes, \"Curst be he that moves my bones\""
             ,"Answer":"William Shakespeare"
             ,"value":["William Shakespeare", "Rudyard Kipling", "William Blake", "Samuel Richardson"]
             ,"Category":"Literature and Media"
@@ -305,7 +305,7 @@ this.questions = [
         },
         {
             "ID":39
-            ,"Question":"\"The Road to Middle Earth\" is a book about this man"
+            ,"question":"\"The Road to Middle Earth\" is a book about this man"
             ,"Answer":"J.R.R. Tolkien"
             ,"value":["JK Rowling", "J.R.R. Tolkien", "Rudyard Kipling", "Enid Blyton"]
             ,"Category":"Literature and Media"
@@ -313,7 +313,7 @@ this.questions = [
         },
         {
             "ID":40
-            ,"Question":"This Russian-born author & scientist who dies in 1992 said, \"I do not fear computers. I fear the lack of them\""
+            ,"question":"This Russian-born author & scientist who dies in 1992 said, \"I do not fear computers. I fear the lack of them\""
             ,"Answer":"Isaac Asimov"
             ,"value":["Isaac Asimov", "Ivan Turgenev", "Leo Tolstoy", "Anton Chekhov"]
             ,"Category":"Literature and Media"
@@ -321,7 +321,7 @@ this.questions = [
         },
         {
             "ID":41
-            ,"Question":"This Disney movie was not created by Walt Disney Animation Studios."
+            ,"question":"This Disney movie was not created by Walt Disney Animation Studios."
             ,"Answer":"A Goofy Movies"
             ,"value":["Frozen", "Hercules", "Chicken Little", "A Goofy Movies"]
             ,"Category":"Literature and Media"
@@ -329,7 +329,7 @@ this.questions = [
         },
         {
             "ID":42
-            ,"Question":"This Oscar category has been discontinued"
+            ,"question":"This Oscar category has been discontinued"
             ,"Answer":"Best Assistant Director"
             ,"value":["Best Original Score", "Best Cinematography", "Best Sound Mixing", "Best Assistant Director"]
             ,"Category":"Literature and Media"
@@ -337,7 +337,7 @@ this.questions = [
         },
         {
             "ID":43
-            ,"Question":"In which Shakespeare play are Stephano and Trinculo characters?"
+            ,"question":"In which Shakespeare play are Stephano and Trinculo characters?"
             ,"Answer":"The Tempest"
             ,"value":["The Tempest", "Hamlet", "As You Like It", "The Merchant of Venice"]
             ,"Category":"Literature and Media"
@@ -345,7 +345,7 @@ this.questions = [
         },
         {
             "ID":44
-            ,"Question":"This country's authors have won more Literature Nobel Prizes than any other, including the US"
+            ,"question":"This country's authors have won more Literature Nobel Prizes than any other, including the US"
             ,"Answer":"France"
             ,"value":["France", "England", "Switzerland", "Greece"]
             ,"Category":"Literature and Media"
@@ -353,7 +353,7 @@ this.questions = [
         },
         {
             "ID":45
-            ,"Question":"In what book does Humpty Dumpty first appear?"
+            ,"question":"In what book does Humpty Dumpty first appear?"
             ,"Answer":"Through the Looking Glass"
             ,"value":["Alice in Wonderland", "Through the Looking Glass", "The Wizard of Oz", "Pippi Longstocking"]
             ,"Category":"Literature and Media"
