4f37350ff24f87d6e44f7b730373610333ef64f5
Carlos Botelho <carlosbpf@gmail.com>
2019-08-19 14:51:21 -0300

    Fixes for cookie creation on etherpad production

--- src/main/resources/plugins/typingEtherpadPlugin/taskWork.js
+++ src/main/resources/plugins/typingEtherpadPlugin/taskWork.js
@@ -50,7 +50,11 @@ function setCookie(name,value,days) {
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    if(window.location.href.indexOf("localhost")!=-1) {
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    } else {
+        document.cookie = name + "=" + (value || "") + expires + "; domain=.pogs.info";
+    }
 }
 function getCookie(name) {
     var nameEQ = name + "=";
