0ac954c8dd66fde38d9626906218ecb853b1504b
Carlos Botelho <carlosbpf@gmail.com>
2020-04-07 20:46:53 -0300

    Fixes for task export still in progress

--- src/main/resources/plugins/surveyPlugin/taskExport.js
+++ src/main/resources/plugins/surveyPlugin/taskExport.js
@@ -8,13 +8,18 @@ var _headerColumns = "";
 
 _headerColumns = "Field Index;Field type;Last subject author; Answer; Ground Truth";
 var answerSheet = null;
+var surveyBluePrint = null;
 
 for(var i=0; i < _taskConfigurationAttributes.length; i++){
     if(_taskConfigurationAttributes[i].attributeName == "answerSheet"){
         answerSheet = JSON.parse(_taskConfigurationAttributes[i].stringValue);
-        break;
+    }
+    if(_taskConfigurationAttributes[i].attributeName == "surveyBluePrint"){
+        surveyBluePrint = JSON.parse(_taskConfigurationAttributes[i].stringValue);
     }
 }
+
+print(JSON.stringify(surveyBluePrint));
 
 for(var i=0; i < _completedTaskAttributes.length; i++){
 
@@ -23,8 +28,27 @@ for(var i=0; i < _completedTaskAttributes.length; i++){
         var rightAnwser = "";
         if(answerSheet){
             rightAnwser = answerSheet[index];
+            print(rightAnwser)
+            print(typeof  rightAnwser);
         }
-        _exportRecordLines.push(index + ";" + _completedTaskAttributes[i].extraData + ";"+_completedTaskAttributes[i].lastAuthorSubject + ";" + _completedTaskAttributes[i].stringValue + ";" + rightAnwser)
+        if(_completedTaskAttributes[i].extraData == "RadioTableField"){
+            var givenAnswersArray = JSON.parse(_completedTaskAttributes[i].stringValue);
+            var groundTruthArray = rightAnwser;
+            for(var k=0; k < givenAnswersArray.length; k ++) {
+                if(givenAnswersArray[k]!="") {
+                    print("(" + groundTruthArray[k] + ")");
+                    _exportRecordLines.push(
+                        index + "." + (k + 1) + ";" + _completedTaskAttributes[i].extraData + ";"
+                        + _completedTaskAttributes[i].lastAuthorSubject + ";"
+                        + givenAnswersArray[k] + ";"
+                        + groundTruthArray[k])
+                }
+            }
+        } else {
+            _exportRecordLines.push(index + ";" + _completedTaskAttributes[i].extraData + ";"
+                                    + _completedTaskAttributes[i].lastAuthorSubject + ";"
+                                    + _completedTaskAttributes[i].stringValue + ";" + rightAnwser)
+        }
     }
 }
 
