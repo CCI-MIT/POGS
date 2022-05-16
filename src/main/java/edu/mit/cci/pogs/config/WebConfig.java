7b63f3451d1f3cbae42eecdfb366c22e9d85a464
Carlos Botelho <carlosbpf@gmail.com>
2022-05-15 23:35:54 -0300

    Upgrade mysql 8 version

--- src/main/java/edu/mit/cci/pogs/config/WebConfig.java
+++ src/main/java/edu/mit/cci/pogs/config/WebConfig.java
@@ -1,11 +1,13 @@
 package edu.mit.cci.pogs.config;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
