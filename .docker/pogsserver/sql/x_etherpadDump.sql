f1b63f08fa67f768fc9c4295e0f3d6aacb278768
Carlos Botelho <carlosbpf@gmail.com>
2022-06-09 17:04:25 -0300

    Wrapped up changes to use newest libraries and mysql 8

--- .docker/pogsserver/sql/x_etherpadDump.sql
+++ .docker/pogsserver/sql/x_etherpadDump.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE etherpad_lite_db CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE etherpad_lite_db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE USER 'etherpad'@'%' IDENTIFIED BY 'a1234';
 GRANT ALL PRIVILEGES ON etherpad_lite_db.* TO 'etherpad'@'%';
 FLUSH PRIVILEGES;
