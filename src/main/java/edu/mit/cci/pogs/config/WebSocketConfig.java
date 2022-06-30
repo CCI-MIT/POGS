5aa2802df1d2c18644fdbd8c7b1334a2cc00852a
Carlos Botelho <carlosbpf@gmail.com>
2022-06-30 19:58:37 -0300

    Bugfixes for new spring update to allow ws connections

--- src/main/java/edu/mit/cci/pogs/config/WebSocketConfig.java
+++ src/main/java/edu/mit/cci/pogs/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
