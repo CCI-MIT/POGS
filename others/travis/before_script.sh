446784a19dc16a7af41dee7741a2934bd3562348
Carlos Botelho <carlosbpf@gmail.com>
2022-08-24 16:51:52 -0300

    New database test in travis

--- others/travis/before_script.sh
+++ others/travis/before_script.sh
@@ -4,5 +4,6 @@ echo "Configuring maven options"
 echo "MAVEN_OPTS='-Xmx1536m -XX:+TieredCompilation -XX:TieredStopAtLevel=1'" > ~/.mavenrc
 
 echo "Running create database script"
+mysql --version
 mysql -e 'CREATE DATABASE IF NOT EXISTS pogs;'
 
